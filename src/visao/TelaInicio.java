9cbeec97fba224cb548c095738d02604fb909d12
Vitória Maria de Novaes <vitoria.m14@aluno.ifsc.edu.br>
2022-11-04 11:54:58 -0300

    telaInicio
    Iniciamos a tela do usuário

--- src/visao/TelaInicio.java
+++ src/visao/TelaInicio.java
@@ -5,10 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class TelaInicio extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -36,6 +40,24 @@ public class TelaInicio extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nome");
+		lblNewLabel.setBounds(99, 13, 27, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(156, 10, 159, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(156, 46, 159, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("CPF");
+		lblNewLabel_1.setBounds(99, 49, 27, 14);
+		contentPane.add(lblNewLabel_1);
 	}
-
 }
