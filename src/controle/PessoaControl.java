26313fb8b623f84e3cd3ab3e6f484464a2680255
SOfia Nascimento <sofia.n06@aluno.ifsc.edu.br>
2022-11-04 10:52:05 -0300

    estudodecaso3

---
+++ src/controle/PessoaControl.java
@@ -0,0 +1,29 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.Pessoa;
+
+public class PessoaControl {
+	private ArrayList<Pessoa> tabelaPessoas;
+	
+	public PessoaControl() { this.tabelaPessoas = new ArrayList<>(); }
+	
+	public boolean inserir(Pessoa p) {
+		
+		return false;
+	}
+	
+	public boolean alterar(Pessoa p) {
+		return false;
+	}
+	
+	public boolean deletar (Pessoa p, int idPessoa) {
+		return false;
+	}
+	
+	public ArrayList<Pessoa> listaPessoa(){
+		
+		return null;
+	}
+}
