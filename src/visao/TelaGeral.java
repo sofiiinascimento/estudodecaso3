282506abd137bfdda0cacea22c56bcf6844978f7
Vitória Novaes <vitoria.m14@aluno.ifsc.edu.br>
2022-11-17 13:54:00 -0300

    Tela Perfil e Tela Geral
    Fiz a Tela de Perfil (Tela Início).
    Observações:
    A quantidade dos livros, é a quantidade dos livros que o usuário cadastrou.
    Os painéis eu usei para exemplificar onde a capa dos livros ficariam.
    Os livros que aparecem na tela são os livros que o usuário cadastrou.
    
    Fiz a Tela Estante (Tela geral)
    Observações:
    Os painéis utilizados são para exemplificar onde a capa dos livros ficariam organizadas.
    
    O status escolhido seria o status que o usuário escolheu para mostrar no comboBox.

--- src/visao/TelaGeral.java
+++ src/visao/TelaGeral.java
@@ -5,6 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import java.awt.Color;
+import java.awt.Font;
 
 public class TelaGeral extends JFrame {
 
@@ -33,9 +37,64 @@ public class TelaGeral extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 250, 250));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(130, 30, 128, 22);
+		contentPane.add(comboBox);
+		
+		JLabel lblNewLabel = new JLabel("Status dos livros");
+		lblNewLabel.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel.setBounds(33, 34, 91, 14);
+		contentPane.add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(240, 230, 140));
+		panel.setBounds(33, 103, 80, 110);
+		contentPane.add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 192, 203));
+		panel_1.setBounds(158, 103, 80, 110);
+		contentPane.add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(135, 206, 250));
+		panel_2.setBounds(303, 103, 80, 106);
+		contentPane.add(panel_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nome do livro");
+		lblNewLabel_1.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(33, 224, 80, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Nome do livro");
+		lblNewLabel_2.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(158, 224, 80, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Nome do livro");
+		lblNewLabel_3.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_3.setBounds(303, 220, 80, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Status escolhido");
+		lblNewLabel_4.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_4.setBounds(33, 237, 91, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Status escolhido");
+		lblNewLabel_5.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_5.setBounds(158, 237, 100, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("Status escolhido");
+		lblNewLabel_6.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_6.setBounds(303, 237, 100, 14);
+		contentPane.add(lblNewLabel_6);
 	}
-
 }
--- src/visao/TelaInicio.java
+++ src/visao/TelaInicio.java
@@ -7,12 +7,18 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.Box;
+import java.awt.Panel;
+import java.awt.Canvas;
+import javax.swing.JEditorPane;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
 
 public class TelaInicio extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JLabel lblNewLabel_1;
 
 	/**
 	 * Launch the application.
@@ -35,29 +41,74 @@ public class TelaInicio extends JFrame {
 	 */
 	public TelaInicio() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 343);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 250, 250));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Nome");
-		lblNewLabel.setBounds(99, 13, 27, 14);
-		contentPane.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setBounds(156, 10, 159, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(156, 46, 159, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNewLabel_1 = new JLabel("CPF");
-		lblNewLabel_1.setBounds(99, 49, 27, 14);
+		lblNewLabel_1 = new JLabel("Quantidade de livros");
+		lblNewLabel_1.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(232, 57, 122, 14);
 		contentPane.add(lblNewLabel_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 224));
+		panel.setForeground(Color.WHITE);
+		panel.setBounds(27, 11, 70, 60);
+		contentPane.add(panel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Biografia do usuário");
+		lblNewLabel_2.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(27, 98, 106, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 255, 224));
+		panel_1.setBounds(0, 119, 434, 14);
+		contentPane.add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(102, 205, 170));
+		panel_2.setBounds(27, 144, 94, 125);
+		contentPane.add(panel_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(new Color(255, 182, 193));
+		panel_3.setBounds(160, 144, 94, 125);
+		contentPane.add(panel_3);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(new Color(152, 251, 152));
+		panel_4.setBounds(290, 144, 94, 125);
+		contentPane.add(panel_4);
+		
+		JLabel lblNewLabel_3 = new JLabel("Nome do livro ");
+		lblNewLabel_3.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_3.setBounds(27, 280, 122, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Nome do livro ");
+		lblNewLabel_4.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_4.setBounds(170, 280, 118, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Nome do livro");
+		lblNewLabel_5.setFont(new Font("Source Sans Pro", Font.BOLD, 12));
+		lblNewLabel_5.setBounds(290, 280, 94, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(new Color(255, 255, 224));
+		panel_5.setBounds(273, 35, 29, 11);
+		contentPane.add(panel_5);
+		
+		JLabel lblNewLabel = new JLabel("Nome do usuário");
+		lblNewLabel.setFont(new Font("Source Sans Pro", Font.BOLD, 14));
+		lblNewLabel.setBackground(new Color(240, 240, 240));
+		lblNewLabel.setBounds(27, 82, 106, 14);
+		contentPane.add(lblNewLabel);
 	}
 }
