584b05f51a4f99216d66c8b6a25274ba14c83436
Sofia Nascimento <sofia.n06@aluno.ifsc.edu.br>
2022-11-18 12:10:14 -0300

    Tela Login

--- src/visao/TelaLogin.java
+++ src/visao/TelaLogin.java
@@ -9,6 +9,10 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 
 public class TelaLogin extends JFrame {
 
@@ -39,20 +43,22 @@ public class TelaLogin extends JFrame {
 	 */
 	public TelaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 570, 562);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setForeground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		txtUsuario = new JTextField();
-		txtUsuario.setBounds(73, 79, 86, 20);
+		txtUsuario.setBounds(73, 212, 143, 20);
 		contentPane.add(txtUsuario);
 		txtUsuario.setColumns(10);
 
 		txtSenha = new JTextField();
-		txtSenha.setBounds(73, 116, 86, 20);
+		txtSenha.setBounds(73, 253, 86, 20);
 		contentPane.add(txtSenha);
 		txtSenha.setColumns(10);
 
@@ -69,7 +75,7 @@ public class TelaLogin extends JFrame {
 				telaGeral.setVisible(true);
 			}
 		});
-		btnLogar.setBounds(73, 164, 89, 23);
+		btnLogar.setBounds(45, 319, 89, 23);
 		contentPane.add(btnLogar);
 
 		btnCadastrar = new JButton("CADASTRAR USUARIO");
@@ -81,7 +87,30 @@ public class TelaLogin extends JFrame {
 				telaCadUsuario.setVisible(true);
 			}
 		});
-		btnCadastrar.setBounds(196, 164, 143, 23);
+		btnCadastrar.setBounds(318, 265, 143, 23);
 		contentPane.add(btnCadastrar);
+		
+		JLabel lblNewLabel = new JLabel("Welcome");
+		lblNewLabel.setForeground(new Color(153, 0, 102));
+		lblNewLabel.setFont(new Font("Vijaya", Font.BOLD, 40));
+		lblNewLabel.setBounds(224, 28, 121, 42);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Seja bem-vindo(a) a sua estante virtual ");
+		lblNewLabel_1.setForeground(new Color(153, 0, 51));
+		lblNewLabel_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
+		lblNewLabel_1.setBounds(138, 56, 332, 54);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Se for sua primeira vez aqui, se cadastre!\r\nPorém, se você for um usuário, \r\nfaça seu login e tenha acesso a sua estante :)");
+		lblNewLabel_2.setBackground(new Color(255, 255, 255));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setLabelFor(this);
+		lblNewLabel_2.setBounds(178, 121, 258, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Nome");
+		lblNewLabel_3.setBounds(10, 215, 46, 14);
+		contentPane.add(lblNewLabel_3);
 	}
 }
