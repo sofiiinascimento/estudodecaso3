36e3295ff6637eb149753d31aca666922adc0db5
Vitória Novaes <vitoria.m14@aluno.ifsc.edu.br>
2022-11-25 11:36:03 -0300

    Arrumei as telas

--- src/visao/TelaCadastroUsuario.java
+++ src/visao/TelaCadastroUsuario.java
@@ -37,74 +37,74 @@ public class TelaCadastroUsuario extends JFrame {
 
 		JLabel lblCadUsuario = new JLabel("CADASTRE-SE");
 		lblCadUsuario.setForeground(new Color(255, 51, 153));
-		lblCadUsuario.setFont(new Font("Segoe Script", Font.PLAIN, 25));
-		lblCadUsuario.setBounds(180, 28, 227, 30);
+		lblCadUsuario.setFont(new Font("Segoe Script", Font.PLAIN, 27));
+		lblCadUsuario.setBounds(155, 50, 227, 30);
 		contentPane.add(lblCadUsuario);
 
 		JLabel lblNome = new JLabel("Nome completo:");
 		lblNome.setForeground(new Color(153, 51, 102));
 		lblNome.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
-		lblNome.setBounds(10, 79, 147, 30);
+		lblNome.setBounds(10, 108, 147, 30);
 		contentPane.add(lblNome);
 
 		textNomeUsuario = new JTextField();
-		textNomeUsuario.setBounds(10, 120, 237, 20);
+		textNomeUsuario.setBounds(10, 145, 237, 20);
 		contentPane.add(textNomeUsuario);
 		textNomeUsuario.setColumns(10);
 
 		JLabel lblCasdDataNasc = new JLabel("Data de nascimento:");
 		lblCasdDataNasc.setForeground(new Color(153, 51, 102));
 		lblCasdDataNasc.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
-		lblCasdDataNasc.setBounds(294, 87, 173, 22);
+		lblCasdDataNasc.setBounds(306, 112, 173, 22);
 		contentPane.add(lblCasdDataNasc);
 
 		textDataNasc = new JTextField();
-		textDataNasc.setBounds(296, 120, 122, 20);
+		textDataNasc.setBounds(306, 145, 122, 20);
 		contentPane.add(textDataNasc);
 		textDataNasc.setColumns(10);
 
 		JLabel lblNewLabel = new JLabel("E-mail:");
 		lblNewLabel.setForeground(new Color(153, 51, 102));
 		lblNewLabel.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
-		lblNewLabel.setBounds(221, 163, 76, 14);
+		lblNewLabel.setBounds(221, 206, 76, 14);
 		contentPane.add(lblNewLabel);
 
 		textEmail = new JTextField();
-		textEmail.setBounds(229, 188, 330, 20);
+		textEmail.setBounds(221, 231, 330, 20);
 		contentPane.add(textEmail);
 		textEmail.setColumns(10);
 
 		JLabel lblCpf = new JLabel("CPF:");
 		lblCpf.setForeground(new Color(153, 51, 102));
 		lblCpf.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
-		lblCpf.setBounds(10, 164, 46, 14);
+		lblCpf.setBounds(10, 206, 46, 14);
 		contentPane.add(lblCpf);
 
 		textCpf = new JTextField();
 		textCpf.setText("");
-		textCpf.setBounds(10, 188, 147, 20);
+		textCpf.setBounds(10, 231, 147, 20);
 		contentPane.add(textCpf);
 		textCpf.setColumns(10);
 
 		JLabel lblNewLabel_1 = new JLabel("UF:");
 		lblNewLabel_1.setForeground(new Color(153, 51, 102));
 		lblNewLabel_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
-		lblNewLabel_1.setBounds(10, 248, 46, 14);
+		lblNewLabel_1.setBounds(10, 298, 46, 14);
 		contentPane.add(lblNewLabel_1);
 
 		JComboBox comboBoxEstado = new JComboBox();
 		comboBoxEstado.setBackground(new Color(255, 255, 204));
-		comboBoxEstado.setBounds(59, 247, 76, 22);
+		comboBoxEstado.setBounds(64, 297, 76, 22);
 		contentPane.add(comboBoxEstado);
 
 		JLabel lblCidade = new JLabel("Cidade:");
 		lblCidade.setForeground(new Color(153, 51, 102));
 		lblCidade.setFont(new Font("Trebuchet MS", Font.PLAIN, 18));
-		lblCidade.setBounds(208, 248, 76, 14);
+		lblCidade.setBounds(192, 305, 76, 14);
 		contentPane.add(lblCidade);
 
 		textField = new JTextField();
-		textField.setBounds(294, 248, 162, 20);
+		textField.setBounds(289, 305, 162, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 
@@ -116,7 +116,7 @@ public class TelaCadastroUsuario extends JFrame {
 		btnNewButton.setBackground(new Color(255, 51, 153));
 		btnNewButton.setForeground(new Color(153, 51, 102));
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnNewButton.setBounds(221, 301, 133, 23);
+		btnNewButton.setBounds(178, 410, 133, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnVoltarLogin = new JButton("VOLTAR");
